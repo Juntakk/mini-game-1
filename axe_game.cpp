f1af06ca434e5d476331878d607d19c3c2112a33
Juntakk <nick.habashi@gmail.com>
2024-02-25 23:04:11 -0500

    Day 4

--- axe_game.cpp
+++ axe_game.cpp
@@ -4,6 +4,7 @@
 struct Player
 {
     Vector2 position;
+    Vector2 speed;
     float radius;
     Color color;
     int hp;
@@ -11,172 +12,271 @@ struct Player
     int exp;
 };
 
-struct Axe
+struct Enemy
 {
-    Rectangle rect;
-    Vector2 speed;
+    Vector2 position;
+    float radius;
     Color color;
+    Vector2 speed;
+    bool isActive;
 };
 
 struct Bullet
 {
     Vector2 position;
     Vector2 speed;
+    int radius;
     Color color;
     bool active;
 };
 
 // Window dimensions
-const int width{1800};
-const int height{900};
+const int screenWidth{1800};
+const int screenHeight{900};
 
 // Main functions
-void UpdatePlayer(Player &player, Bullet bullets[], int maxBullets);
-void UpdateAxes(Axe &axe);
-bool CheckCollision(Player &player, Axe &axe);
+void UpdatePlayer(Player &player, Bullet bullets[], int maxBullets, float dashDistance);
 void UpdateBullets(Bullet bullets[], int maxBullets);
-void DrawBullets(const Bullet bullets[], int maxBullets);
+void DrawBullets(const Bullet bullets[], int maxBullets, int bulletRadius);
 void SpawnBullets(Bullet bullets[], int maxBullets, float &bulletTimer, float bulletInterval, Player player);
-void HandleBulletAxeCollision(Bullet &bullet, Axe &axe, Player &player, Sound &sound);
-bool CheckBulletAxeCollision(const Bullet &bullet, const Axe &axe);
+void PlayerDash(Player &player, float distance, Vector2 dashDirection);
+void DrawEnemies(Enemy enemies[], int maxEnemies);
+void InitiateEnemies(Enemy enemies[], int maxEnemies);
+void DrawPlayer(Player &player);
+void UpdateEnemies(Enemy enemies[], int maxEnemies, Player &player, Bullet bullets[], int maxBullets);
+bool CheckBulletEnemyCollision(const Bullet &bullet, const Enemy &enemy);
+void HandleBulletEnemyCollision(Bullet &bullet, Enemy &enemy);
+void TriggerBulletEnemyCollision(Player &player, Bullet bullets[], Enemy enemies[], int maxBullets, int maxEnemies);
+void HandleExp(Player &player);
 
+// MAIN
 int main()
 {
-    InitWindow(width, height, "Avoid the obstacles");
+    InitWindow(screenWidth, screenHeight, "Game");
     InitAudioDevice();
 
-    Vector2 playerPos = {height / 2, width / 2};
+    // VARIABLES
+    // Player
+    Vector2 playerPos = {screenWidth / 2, screenHeight / 2};
+    Vector2 playerSpeed = {0, 0};
+    float dashDistance = 44.0f;
 
+    // Sound
+    Sound sound = LoadSound("sounds/level_up.wav");
+
+    // Enemies
+    const int maxEnemies = 30;
+    Enemy enemies[maxEnemies];
+
+    // State
+    Color backgroundColor = GRAY;
+    int currentLevel = 1;
+
+    // Bullets
+    const int maxBullets = 8;
+    Bullet bullets[maxBullets] = {0};
+    const int bulletRadius = 10;
+    float bulletTimer = 0.0f;
+    float bulletInterval = 0.7f;
+
+    // Initiate player
     Player player = {
         playerPos,
+        playerSpeed,
         20,
         BLACK,
         100,
         1,
         0};
 
-    Sound sound = LoadSound("sounds/level_up.wav");
-    const int num_axes = 40;
-    Axe axes[num_axes];
-    const int maxBullets = 4;
-    Bullet bullets[maxBullets] = {0};
-    float bulletTimer = 0.0f;
-    float bulletInterval = 0.8f;
+    // Initiate enemies
+    InitiateEnemies(enemies, maxEnemies);
 
-    for (int i = 0; i < num_axes; ++i)
-    {
-        axes[i].rect.width = 40;
-        axes[i].rect.height = 40;
-        axes[i].rect.x = GetRandomValue(0, width);
-        axes[i].rect.y = GetRandomValue(0, height);
-        axes[i].speed.x = GetRandomValue(-3, 3);
-        axes[i].speed.y = GetRandomValue(-3, 3);
-        axes[i].color = RED;
-    }
-
+    // MAIN LOOP
     SetTargetFPS(60);
-
     while (!WindowShouldClose())
     {
-        UpdatePlayer(player, bullets, maxBullets);
-        DrawBullets(bullets, maxBullets);
-        UpdateBullets(bullets, maxBullets);
-        SpawnBullets(bullets, maxBullets, bulletTimer, bulletInterval, player);
-
-        for (int i = 0; i < maxBullets; ++i)
-        {
-            if (bullets[i].active)
-            {
-                for (int j = 0; j < num_axes; ++j)
-                {
-                    if (CheckBulletAxeCollision(bullets[i], axes[j]))
-                    {
-                        HandleBulletAxeCollision(bullets[i], axes[j], player, sound);
-                    }
-                }
-            }
-        }
-
-        for (int i = 0; i < num_axes; i++)
-        {
-            UpdateAxes(axes[i]);
-
-            // Player getting hit
-            if (CheckCollision(player, axes[i]))
-            {
-                player.position = playerPos;
-            }
-        }
-
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(backgroundColor);
+
+        // Bullets
+        DrawBullets(bullets, maxBullets, bulletRadius);
+        SpawnBullets(bullets, maxBullets, bulletTimer, bulletInterval, player);
+        UpdateBullets(bullets, maxBullets);
+
+        // Player
+        DrawPlayer(player);
+        HandleExp(player);
+        UpdatePlayer(player, bullets, maxBullets, dashDistance);
+
+        // Enemies
+        DrawEnemies(enemies, maxEnemies);
+        UpdateEnemies(enemies, maxEnemies, player, bullets, maxBullets);
+
+        // Collisions
+        TriggerBulletEnemyCollision(player, bullets, enemies, maxBullets, maxEnemies);
 
         // Draw UI
-        DrawText(TextFormat("Level: %d", player.lvl), 10, 10, 30, DARKGRAY);
-        DrawText(TextFormat("Experience: %d", player.exp), 10, 40, 30, DARKGRAY);
-        DrawText(TextFormat("HP: %d", player.hp), 10, 70, 30, DARKGRAY);
-
-        // Draw player
-        DrawCircleV(player.position, player.radius, player.color);
-
-        // Draw axes
-        for (int i = 0; i < num_axes; i++)
-        {
-            DrawRectangleRec(axes[i].rect, axes[i].color);
-        }
+        DrawText(TextFormat("Level: %d", player.lvl), 10, 10, 30, ORANGE);
+        DrawText(TextFormat("Experience: %d", player.exp), 10, 40, 30, ORANGE);
+        DrawText(TextFormat("HP: %d", player.hp), 10, 70, 30, ORANGE);
+        DrawText(TextFormat("Stage: %d", currentLevel), 800, 40, 40, BLACK);
 
         EndDrawing();
     }
+
     UnloadSound(sound);
     CloseAudioDevice();
     CloseWindow();
 }
 
-// Player movement
-void UpdatePlayer(Player &player, Bullet bullets[], int maxBullets)
+// Player
+void UpdatePlayer(Player &player, Bullet bullets[], int maxBullets, float dashDistance)
 {
-    // Move player with arrow keys
+    Vector2 movementInput = {0, 0};
+
     if (IsKeyDown(KEY_RIGHT))
-        player.position.x += 5;
+    {
+        movementInput.x += 1;
+    }
+
     if (IsKeyDown(KEY_LEFT))
-        player.position.x -= 5;
+    {
+        movementInput.x -= 1;
+    }
+
     if (IsKeyDown(KEY_DOWN))
-        player.position.y += 5;
+    {
+        movementInput.y += 1;
+    }
+
     if (IsKeyDown(KEY_UP))
-        player.position.y -= 5;
+    {
+        movementInput.y -= 1;
+    }
+
+    // Dash when space bar is pressed
+    if (IsKeyPressed(KEY_SPACE))
+    {
+        // Use the movement input as the dash direction
+        PlayerDash(player, 50.0f, movementInput);
+    }
+
+    // Normalize the movement input to ensure consistent speed
+    movementInput = Vector2Normalize(movementInput);
+
+    // Update player speed based on the movement input
+    player.speed = Vector2Scale(movementInput, 5); // Adjust speed as needed
+
+    // Update player position based on the speed vector
+    player.position.x += player.speed.x;
+    player.position.y += player.speed.y;
 
     // Ensure player stays within the screen bounds
-    player.position.x = Clamp(player.position.x, player.radius, width - player.radius);
-    player.position.y = Clamp(player.position.y, player.radius, height - player.radius);
+    player.position.x = Clamp(player.position.x, player.radius, screenWidth - player.radius);
+    player.position.y = Clamp(player.position.y, player.radius, screenHeight - player.radius);
 }
-
-// Enemy movement
-void UpdateAxes(Axe &axe)
+void PlayerDash(Player &player, float dashDistance, Vector2 dashDirection)
 {
-    // Move the Axes
-    axe.rect.x += axe.speed.x;
-    axe.rect.y += axe.speed.y;
+    // Store the current speed for later restoration
+    Vector2 originalSpeed = player.speed;
 
-    // Bounce off the screen edges
-    if ((axe.rect.x <= 0) || (axe.rect.x >= width - axe.rect.width))
-        axe.speed.x *= -1;
-    if ((axe.rect.y <= 0) || (axe.rect.y >= height - axe.rect.height))
-        axe.speed.y *= -1;
+    // Normalize the dash direction to ensure consistent dash distance
+    dashDirection = Vector2Normalize(dashDirection);
+
+    // Calculate the dash vector based on the normalized dash direction
+    Vector2 dashVector = Vector2Scale(dashDirection, dashDistance);
+
+    // Apply the dash vector to the player's position
+    player.position = Vector2Add(player.position, dashVector);
+
+    // Restore the original speed
+    player.speed = originalSpeed;
 }
-
-// Collision between player and enemy
-bool CheckCollision(Player &player, Axe &axe)
+void DrawPlayer(Player &player)
 {
-    Vector2 closestPoint;
-    closestPoint.x = Clamp(player.position.x, axe.rect.x, axe.rect.x + axe.rect.width);
-    closestPoint.y = Clamp(player.position.y, axe.rect.y, axe.rect.y + axe.rect.height);
-
-    float distance = Vector2Distance(player.position, closestPoint);
-
-    return distance < player.radius;
+    DrawCircleV(player.position, player.radius, player.color);
+}
+void HandleExp(Player &player)
+{
+    if (player.exp == 100)
+    {
+        player.lvl++;
+        player.exp = 0;
+    }
 }
 
-// Bullet movement
+// Enemies
+void InitiateEnemies(Enemy enemies[], int maxEnemies)
+{
+    for (int i = 0; i < maxEnemies; ++i)
+    {
+        enemies[i].position = {GetRandomValue(0, screenWidth), GetRandomValue(0, screenHeight)};
+        enemies[i].radius = 30;
+        enemies[i].color = {GetRandomValue(0, 255), GetRandomValue(0, 255), GetRandomValue(0, 255), 255};
+        enemies[i].speed = {GetRandomValue(-4, 4),
+                            GetRandomValue(-4, 4)};
+    }
+}
+void DrawEnemies(Enemy enemies[], int maxEnemies)
+{
+    for (int i = 0; i < maxEnemies; i++)
+    {
+        DrawCircleV(enemies[i].position, enemies[i].radius, enemies[i].color);
+    }
+}
+void UpdateEnemies(Enemy enemies[], int maxEnemies, Player &player, Bullet bullets[], int maxBullets)
+{
+    int activeEnemies = 0;
+
+    // Enemy movement
+    for (int i = 0; i < maxEnemies; i++)
+    {
+        activeEnemies++;
+        enemies[i].position.x += enemies[i].speed.x;
+        enemies[i].position.y += enemies[i].speed.y;
+
+        // Bouncing on Walls
+        if (enemies[i].position.x <= 0 + enemies[i].radius || enemies[i].position.x >= screenWidth - enemies[i].radius)
+        {
+            enemies[i].speed.x *= -1;
+        }
+        if (enemies[i].position.y >= screenHeight - enemies[i].radius || enemies[i].position.y <= 0 + enemies[i].radius)
+        {
+            enemies[i].speed.y *= -1;
+        }
+    }
+}
+
+// Collisions
+bool CheckBulletEnemyCollision(const Bullet &bullet, const Enemy &enemy)
+{
+    return (CheckCollisionCircles(bullet.position, bullet.radius, enemy.position, enemy.radius));
+}
+void HandleBulletEnemyCollision(Bullet &bullet, Enemy &enemy)
+{
+    bullet.active = false;
+    enemy.position.x = -100;
+}
+void TriggerBulletEnemyCollision(Player &player, Bullet bullets[], Enemy enemies[], int maxBullets, int maxEnemies)
+{
+    for (int i = 0; i < maxBullets; ++i)
+    {
+        if (bullets[i].active)
+        {
+            for (int j = 0; j < maxEnemies; ++j)
+            {
+                if (CheckBulletEnemyCollision(bullets[i], enemies[j]))
+                {
+                    HandleBulletEnemyCollision(bullets[i], enemies[j]);
+                    player.exp += 10;
+                }
+            }
+        }
+    }
+}
+
+// Bullets
 void UpdateBullets(Bullet bullets[], int maxBullets)
 {
     for (int i = 0; i < maxBullets; ++i)
@@ -187,28 +287,24 @@ void UpdateBullets(Bullet bullets[], int maxBullets)
             bullets[i].position.y += bullets[i].speed.y;
 
             // Deactivate bullet if it goes out of screen bounds
-            if (bullets[i].position.y < 0 || bullets[i].position.y > height ||
-                bullets[i].position.x < 0 || bullets[i].position.x > width)
+            if (bullets[i].position.y < 0 || bullets[i].position.y > screenHeight ||
+                bullets[i].position.x < 0 || bullets[i].position.x > screenWidth)
             {
                 bullets[i].active = false;
             }
         }
     }
 }
-
-// Bullet aesthetic
-void DrawBullets(const Bullet bullets[], int maxBullets)
+void DrawBullets(const Bullet bullets[], int maxBullets, int bulletRadius)
 {
     for (int i = 0; i < maxBullets; ++i)
     {
         if (bullets[i].active)
         {
-            DrawCircleV(bullets[i].position, 10, bullets[i].color);
+            DrawCircleV(bullets[i].position, bulletRadius, bullets[i].color);
         }
     }
 }
-
-// Bullet spawn
 void SpawnBullets(Bullet bullets[], int maxBullets, float &bulletTimer, float bulletInterval, Player player)
 {
     bulletTimer += GetFrameTime();
@@ -222,39 +318,16 @@ void SpawnBullets(Bullet bullets[], int maxBullets, float &bulletTimer, float bu
             bullets[i].color = DARKGRAY; // Adjust bullet color as needed
 
             // Initialize bullets with different directions
-            bullets[0].speed = {0, -10}; // Up
-            bullets[1].speed = {0, 10};  // Down
-            bullets[2].speed = {-10, 0}; // Left
-            bullets[3].speed = {10, 0};  // Right
+            bullets[0].speed = {0, -10};   // Up
+            bullets[1].speed = {0, 10};    // Down
+            bullets[2].speed = {-10, 0};   // Left
+            bullets[3].speed = {10, 0};    // Right
+            bullets[4].speed = {10, -10};  // Up/Right
+            bullets[5].speed = {10, 10};   // Down/Right
+            bullets[6].speed = {-10, -10}; // Up/Left
+            bullets[7].speed = {-10, 10};  // Down/Left
         }
 
         bulletTimer = 0.0f;
-    }
-}
-
-// Collision between bullet and enemy
-bool CheckBulletAxeCollision(const Bullet &bullet, const Axe &axe)
-{
-    // Check if the bullet's position is inside the rectangle of the axe
-    return (bullet.position.x >= axe.rect.x &&
-            bullet.position.x <= axe.rect.x + axe.rect.width &&
-            bullet.position.y >= axe.rect.y &&
-            bullet.position.y <= axe.rect.y + axe.rect.height);
-}
-void HandleBulletAxeCollision(Bullet &bullet, Axe &axe, Player &player, Sound &sound)
-{
-    // Deactivate the bullet
-    bullet.active = false;
-
-    // Deactivate the axe
-    axe.rect.x = -100; // Move axe off-screen to "deactivate" it
-
-    // Exp gain
-    player.exp += 10;
-    if (player.exp == 100)
-    {
-        player.exp = 0;
-        player.lvl += 1;
-        PlaySound(sound);
     }
 }
